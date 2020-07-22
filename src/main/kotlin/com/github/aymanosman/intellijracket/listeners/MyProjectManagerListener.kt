b19a1bf153bb4666e4c6e9e8b00e83780ab4926d
GitHub Action <action@github.com>
2020-07-22 19:34:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/aymanosman/intellijracket/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/aymanosman/intellijracket/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.aymanosman.intellijracket.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.aymanosman.intellijracket.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/aymanosman/intellijracket/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/aymanosman/intellijracket/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.aymanosman.intellijracket.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aymanosman.intellijracket.MyBundle
 
 class MyApplicationService {
 
