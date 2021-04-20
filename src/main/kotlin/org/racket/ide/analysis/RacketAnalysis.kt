ca2a355f9ec6223e35362943d10cf0e891c603a1
Ayman Osman <aymano.osman@gmail.com>
2021-04-20 13:46:03 +0100

    RacketAnalysis

--- src/main/kotlin/org/racket/ide/analysis/RacketAnalysis.kt
+++ src/main/kotlin/org/racket/ide/analysis/RacketAnalysis.kt
@@ -14,87 +14,83 @@ import org.racket.lang.core.psi.RacketElementTypes
 import java.nio.charset.StandardCharsets
 
 class RacketAnalysis {
-  companion object {
+    companion object {
 
-      fun unusedRequires(file: PsiFile): Collection<Problem>? {
-          val racketPath = RacketSdkUtil.getRacketPath(file.project) ?: return null
+        fun unusedRequires(file: PsiFile): Collection<Problem>? {
+            return runCommand("intellij/unused-requires", file)
+        }
 
-          val commandLine: GeneralCommandLine = GeneralCommandLine()
-                  .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
-                  .withWorkDirectory(file.virtualFile.parent.path)
-                  .withExePath(racketPath)
-                  .withParameters(listOf("-lm", "intellij/unused-requires", "--", "--stdin", file.virtualFile.name))
-          val handler = OSProcessHandler(commandLine)
+        fun lintWithExpand(file: PsiFile): Collection<Problem>? {
+            return runCommand("intellij/lint-with-expand", file)
+        }
 
-          return getOutputProblems(handler, file.text.toByteArray(StandardCharsets.UTF_8))
-      }
+        private fun runCommand(command: String, file: PsiFile): Collection<Problem>? {
+            val racketPath = RacketSdkUtil.getRacketPath(file.project) ?: return null
 
-      fun lintWithExpand(file: PsiFile): Collection<Problem>? {
-          val racketPath = RacketSdkUtil.getRacketPath(file.project) ?: return null
+            val commandLine: GeneralCommandLine = GeneralCommandLine()
+                    .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
+                    .withWorkDirectory(file.virtualFile.parent.path)
+                    .withExePath(racketPath)
+                    .withParameters(listOf("-lm", command, "--", "--stdin", file.virtualFile.name))
+            val handler = OSProcessHandler(commandLine)
 
-          val commandLine: GeneralCommandLine = GeneralCommandLine()
-                  .withParentEnvironmentType(GeneralCommandLine.ParentEnvironmentType.CONSOLE)
-                  .withWorkDirectory(file.virtualFile.parent.path)
-                  .withExePath(racketPath)
-                  .withParameters(listOf("-lm", "intellij/lint-with-expand", "--", "--stdin", file.virtualFile.name))
-          val handler = OSProcessHandler(commandLine)
+            return getOutputProblems(handler, file.text.toByteArray(StandardCharsets.UTF_8))
+        }
 
-          return getOutputProblems(handler, file.text.toByteArray(StandardCharsets.UTF_8))
-      }
+        private fun getOutputProblems(handler: OSProcessHandler, stdin: ByteArray): Collection<Problem>? {
+            val response: Ref<Collection<Problem>> = Ref.create()
+            handler.addProcessListener(object : CapturingProcessAdapter() {
+                override fun processTerminated(event: ProcessEvent) {
+                    when (event.exitCode) {
+                        0 -> {
+                            response.set(output.stdoutLines.mapNotNull { line ->
+                                parseProblem(line)
+                            })
+                        }
+                        137 -> {
+                            LOG.warn("Racket analysis process terminated")
+                        }
+                        else -> {
+                            RacketSdkUtil.checkRacketIntellijPackageInstalled()
+                        }
+                    }
+                }
+            })
 
-      private fun getOutputProblems(handler: OSProcessHandler, stdin: ByteArray): Collection<Problem>? {
-          val response: Ref<Collection<Problem>> = Ref.create()
-          handler.addProcessListener(object : CapturingProcessAdapter() {
-              override fun processTerminated(event: ProcessEvent) {
-                  when (event.exitCode) {
-                      0 -> {
-                          response.set(output.stdoutLines.map { line ->
-                              parseProblem(line)
-                          })
-                      }
-                      137 -> {
-                          LOG.warn("Racket analysis process terminated")
-                      }
-                      else -> {
-                          RacketSdkUtil.checkRacketIntellijPackageInstalled()
-                      }
-                  }
-              }
-          })
+            handler.startNotify()
+            handler.processInput.write(stdin)
+            handler.processInput.close()
 
-          handler.startNotify()
-          handler.processInput.write(stdin)
-          handler.processInput.close()
+            if (!handler.waitFor(TIMEOUT_IN_MILLISECONDS.toLong())) {
+                handler.destroyProcess()
+            }
 
-          if (!handler.waitFor(TIMEOUT_IN_MILLISECONDS.toLong())) {
-              handler.destroyProcess()
-          }
+            return response.get()
+        }
 
-          return response.get()
-      }
+        private fun parseProblem(line: String): Problem? {
+            val lexer = RacketLexerAdapter()
+            lexer.start(line)
 
-      private fun parseProblem(line: String): Problem {
-          val lexer = RacketLexerAdapter()
-          lexer.start(line)
+            val offset = if (lexer.tokenType == RacketElementTypes.NUMBER) lexer.tokenText.toInt() else return null
 
-          assert(lexer.tokenType == RacketElementTypes.NUMBER)
-          val offset = lexer.tokenText.toInt()
+            lexer.advance()
 
-          lexer.advance()
+            while (lexer.tokenType == TokenType.WHITE_SPACE) {
+                lexer.advance()
+            }
 
-          while (lexer.tokenType == TokenType.WHITE_SPACE) {
-              lexer.advance()
-          }
+            val message =
+                    if (lexer.tokenType == RacketElementTypes.STRING)
+                        lexer.tokenText // TODO remove quotes
+                    else "error encountered"
 
-          assert(lexer.tokenType == RacketElementTypes.STRING)
-          val message = lexer.tokenText // TODO remove quotes
+            return Problem(offset, message)
+        }
 
-          return Problem(offset, message)
-      }
+        private const val TIMEOUT_IN_MILLISECONDS = 10000
+        private val LOG = Logger.getInstance(RacketAnalysis::class.java)
+    }
 
-      private const val TIMEOUT_IN_MILLISECONDS = 10000
-      private val LOG = Logger.getInstance(RacketAnalysis::class.java)
-  }
-
-    data class Problem(val offset : Int, val message: String)
+    data class Problem(val offset: Int, val message: String)
 }
