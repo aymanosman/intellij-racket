6e6aedbd3eb50f17b2bbed35c5c6ed58401a32ea
Robby Ciliberto <robby@ciliber.to>
2022-10-16 04:40:43 -0400

    Update Racket File Icon
    Updated Racket icon for files with a more JetBrains-like icon.
    
    Signed-off-by: Robby Ciliberto <robby@ciliber.to>

--- src/main/kotlin/org/racket/lang/core/RacketIcons.kt
+++ src/main/kotlin/org/racket/lang/core/RacketIcons.kt
@@ -3,12 +3,19 @@ package org.racket.lang.core
 import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
+/**
+ * Racket plugin icons.
+ */
 object RacketIcons {
-    // File icon (Scaled down Racket logo)
-    val RACKET_FILE = getIcon("/icons/racket-icons/racket-file.svg")
+    // Logos
+    val RACKET = getIcon("/icons/racket/racket.svg")
+
+    // File types
+    val RACKET_FILE = getIcon("/icons/racket-file.svg")
 
     /**
      * Helper function to load icons from the filesystem.
-     * @param path  Path to the icon on the filesystem.*/
+     * @param path  Path to the icon on the filesystem.
+     * */
     private fun getIcon(path: String): Icon = IconLoader.getIcon(path, RacketIcons::class.java)
 }
